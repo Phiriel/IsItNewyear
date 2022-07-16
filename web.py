ce1b00a4b0b75167a71c444dbdebbb61b575a36e
Phiriel7 <philipgabriel21@gmail.com>
2022-07-16 17:54:54 +0530

    First Commit

--- web.py
+++ web.py
@@ -6,8 +6,8 @@ app = Flask(__name__, template_folder="template")
 
 @app.route('/')
 def hello():
-    now = datetime.datetime.now()
-    newyear = ((now.day==1) and (now.month==1))
+    a = datetime.datetime.now()
+    newyear = ((a.day==1) and (a.month==1))
     return render_template('hello.html',newyear=newyear)
 
 app.run(debug=True)
